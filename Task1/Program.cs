85f45c720ea89d156e0d013a7a35630851b679fa
lllypik <blesphoton@gmail.com>
2022-07-31 14:35:59 +0300

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите число N для вычисления N!");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            // Запуск асинхронного метода
+            GetFactorialAsync(amount);
+
+            //Запуск метода в основном потоке для проверки работы
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(100);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void GetFactorial(int amount)
+        {
+            int result = 1;
+            for (int i = 1; i <= amount; i++)
+            {
+                result *= i;
+                Thread.Sleep(50);
+            }
+            Console.WriteLine($"Факториал числ {amount} равен {result}");
+        }
+
+        static async void GetFactorialAsync(int amount)
+        {
+            await Task.Run(() => GetFactorial(amount));
+        }
+
+
+    }
+}
